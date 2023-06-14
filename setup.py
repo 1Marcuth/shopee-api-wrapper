5e2b07ce39192c43f8145ea6624067ec4dcca31c
TantoFaz <thiago.playgames15@gmail.com>
2023-06-14 15:37:37 -0300

    version 1.1

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as file:
 
 setup(
     name="shopee-api-wrapper",
-    version="0.1.0",
+    version="0.1.1",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
     long_description_content_type="text/markdown",
     author_email="marcuth2006@gmail.com",
-    keywords="amazon affiliate scraper",
+    keywords="shopee api crawler scraper product shop",
     description="A simple library that communicates with the Shopee website API.",
     packages=["shopee_api"] + [ "shopee_api/" + x for x in find_packages("shopee_api") ]
 )
